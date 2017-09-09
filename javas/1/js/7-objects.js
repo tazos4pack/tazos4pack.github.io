ee6c3e99ef100d023e4f531d7cbf33db37866010
wasiek <mateusz.wasiucionek@gmail.com>
2017-09-09 14:15:30 +0200

    js obiekty

--- javas/1/js/7-objects.js
+++ javas/1/js/7-objects.js
@@ -1,1 +1,52 @@
 'use strict';
+
+var kaja = {
+    imie: "Kaja",
+    wzrost: 152,
+    przedstawOsobe: function () {
+        console.log(this.imie)
+    }
+}
+
+var krystian = {
+    imie: "Krystian",
+    wzrost: 180,
+    przedstawOsobe: function () {
+        console.log(this.imie)
+    }
+}
+
+console.log(kaja);
+console.log(kaja.wzrost);
+console.log(kaja["wzrost"]);
+
+console.log(krystian["wzrost"]);
+
+kaja.przedstawOsobe();
+krystian.przedstawOsobe();
+
+
+class Osoba {
+    constructor(firstName, lastName, height, eyes) {
+        this.imie = firstName;
+        this.nazwisko = lastName;
+        this.wzrost = height;
+        this.oczy = eyes;
+    }
+
+    wyswietlInfo() {
+        console.log("Imię: " + this.imie + "\n" + "Nazwisko: " + this.nazwisko + "\n" + "Wzrost: " + this.wzrost + "\n" + "Oczy: " + this.oczy);
+    }
+}
+
+var krystian = new Osoba("Krystian", "Dziopa", 180, "Niebieskie");
+var kaja = new Osoba("Kaja", "Brzęczyszczykiewicz", 250, "Piękne");
+console.log(krystian);
+console.log(kaja);
+
+krystian.wyswietlInfo();
+kaja.wyswietlInfo();
+
+// JAK NIE DODAWAĆ:
+//krystian.wzrost = 180;
+//krystian.oczy = "niebieskie";

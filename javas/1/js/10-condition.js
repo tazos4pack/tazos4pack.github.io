91297c7ab7cb5880f9b3e64778841dca427ed6bd
wasiek <mateusz.wasiucionek@gmail.com>
2017-09-09 15:16:35 +0200

    js conditions

---
+++ javas/1/js/10-condition.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var wzrostMateusz = 190;
+var wzrostOlga = 160;
+
+// IF
+//if (wzrostOlga < wzrostMateusz) {
+//    console.log("Olga jest niższa niż Mateusz.");
+//}
+
+// IF ELSE
+//if (wzrostOlga < wzrostMateusz) {
+//    console.log("Olga jest niższa niż Mateusz.");
+//} else {
+//    console.log("Olga jest wyższa od Mateusza.");
+//}
+
+// ELSE IF
+//if (wzrostOlga < wzrostMateusz) {
+//    console.log("Olga jest niższa niż Mateusz.");
+//} else if (wzrostOlga == wzrostMateusz) {
+//    console.log("Olga i Mateusz są tego samego wzrostu.");
+//} else {
+//    console.log("Olga jest wyższa od Mateusza.");
+//}
+
+
+// SWITCH
+var kolor = "różowy";
+
+switch (kolor) {
+    case "czerwony":
+        console.log("Kolor czerwony")
+        break;
+    case "zielony":
+        console.log("Kolor zielony")
+        break;
+    case "różowy":
+        console.log("Kolor różowy")
+        break;
+    default:
+        console.log("Inny kolor")
+}

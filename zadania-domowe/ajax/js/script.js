bbea781b33c027d2a3f62f69304668acb3bb33e4
wasiek <mateusz.wasiucionek@gmail.com>
2017-09-22 20:06:04 +0200

    poprawka + clean zadania domowego ajax

--- zadania-domowe/ajax/js/script.js
+++ zadania-domowe/ajax/js/script.js
@@ -1,13 +1,3 @@
-//$(function () {
-//    $('button').on('click', function () {
-//        $('body').append('<div id="dane-programisty"></div>');
-//        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (dane) {
-//            $('<p><strong>Imię:</strong> ' + dane.imie, '<p><strong>Nazwisko:</strong> ' + dane.nazwisko, '<p><strong>Zawód:</strong> ' + dane.zawod, '<p><strong>Firma:</strong> ' + dane.firma).appendTo('#dane-programisty');
-//        })
-//    })
-//})
-
-
 $(function () {
     $('button').on('click', function () {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (dane) {
@@ -15,13 +5,3 @@ $(function () {
         })
     })
 })
-
-
-//$(function () {
-//    $('button').on('click', function () {
-//        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (dane) {
-//            $('body').append('<div id="dane-programisty"></div>');
-//            $('<p><strong>Imię:</strong> ' + dane.imie, '</p><p><strong>Nazwisko:</strong> ' + dane.nazwisko, '</p><p><strong>Zawód:</strong> ' + dane.zawod, '</p><p><strong>Firma:</strong> ' + dane.firma, '</p>').appendTo('#dane-programisty');
-//        })
-//    })
-//})

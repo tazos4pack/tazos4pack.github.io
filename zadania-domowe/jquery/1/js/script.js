aadb03c12f5581a88773fea7d612382c88d99cac
wasiek <mateusz.wasiucionek@gmail.com>
2017-09-22 22:28:55 +0200

    jędrek

---
+++ zadania-domowe/jquery/1/js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('h2').hide();
+    $('#start-animacji').click(function () {
+        $('#kwadrat').animate({
+            marginLeft: '100px',
+            height: '100px',
+            width: '100px'
+        }, 3000)
+        $('#kwadrat').animate({
+            backgroundColor: '#00F'
+        }, 5000)
+        $('h2').show();
+    });
+});

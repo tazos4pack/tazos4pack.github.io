fe4ee45cc01e448c0bd9fdc7680e40e8365b7775
wasiek <mateusz.wasiucionek@gmail.com>
2018-08-14 21:52:24 +0200

    new projects and update on github page

---
+++ javas/1/js/zad1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function  obliczenia(a, b, c) {
+    var iloczyn = a * b * c;
+    return(iloczyn);
+}
+
+var wynik = obliczenia(1, 2, 3);
+console.log(wynik);
---
+++ javas/1/js/zad2.js
@@ -0,0 +1,10 @@
+function wspak(a) {
+    a.reverse();
+    console.log(a);
+}
+
+var slowo = ['G', 'u', 'ś', 'k', 'a', '!'];
+console.log(wspak(slowo));
+
+
+
